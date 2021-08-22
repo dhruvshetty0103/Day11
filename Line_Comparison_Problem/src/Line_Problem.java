9b25eedf438f867144503d98da6a73a58867e092
dhruvshetty0103 <dhruvshetty0103@gmail.com>
2021-08-22 10:31:46 +0530

    Modified:Code with equals and compareTo functions

--- Line_Comparison_Problem/src/Line_Problem.java
+++ Line_Comparison_Problem/src/Line_Problem.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 class Distance
 {
 	Scanner reader=new Scanner(System.in);
-	double read()
+	double read()//for reading points
 	{
 		
 		double point=reader.nextDouble();
 		
 		return point;
 	}
-	double Distance_Calculation(double x1,double y1,double x2,double y2)
+	double Distance_Calculation(double x1,double y1,double x2,double y2)//distance calculation
 	{
 		
-		return (double)Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+		return (double)Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));//returning the calculated value
 	}
 }
 
@@ -21,7 +21,7 @@ public class Line_Problem
 {
 	public static void main(String[] args) 
 	{
-		Distance d=new Distance();
+		Distance d=new Distance();//Object of distance class created
 		double x1,x2,y1,y2,length_of_line1,length_of_line2;
 		System.out.println("Welccome to Line Comparison Computation");
 		System.out.println("Enter end points of first line (x1,y1) and (x2,y2)");
@@ -30,7 +30,7 @@ public class Line_Problem
 		x2=d.read();
 		y2=d.read();
 		length_of_line1=d.Distance_Calculation(x1, y1, x2, y2);
-		Double l1=new Double(length_of_line1);
+		Double l1=new Double(length_of_line1);//Double object created so that equals and compareTo functions can be used
 		System.out.println("Enter end points of first line (x3,y3) and (x4,y4)");
 		x1=d.read();
 		y1=d.read();
@@ -38,7 +38,7 @@ public class Line_Problem
 		y2=d.read();
 		length_of_line2=d.Distance_Calculation(x1, y1, x2, y2);
 		Double l2=new Double(length_of_line2);
-		if(l1.equals(l2))
+		if(l1.equals(l2))//when both are equal it returns 1
 			System.out.println("Length of both lines are equal and it's length:"+length_of_line1);
 		else
 		{
@@ -49,7 +49,7 @@ public class Line_Problem
 				System.out.println("Line 1 length:"+length_of_line1);
 				System.out.println("Line 2 length:"+length_of_line2);
 			}
-			else
+			else//else the value of result is -1 as for equality equals is used
 			{
 				System.out.println("Line 2 is greater than line 1");
 				System.out.println("Line 2 length:"+length_of_line2);
